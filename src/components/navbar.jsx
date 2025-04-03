501db086b95b624e3521c13993e0595aeb30c249
deHypeChief <dev.hype7@gmail.com>
2025-04-03 18:57:56 +0100

    done 3

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Button from "./button";
 import Eye from "./eye";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 
 export default function Navbar() {
     const [open, setOpen] = useState(false)
@@ -19,6 +21,17 @@ export default function Navbar() {
         console.log(open)
         setOpen(!open)
     }
+
+    const currentHour = new Date().getHours();
+    const greeting =
+        currentHour < 12
+            ? "Good Morning"
+            : currentHour < 18
+                ? "Good Afternoon"
+                : "Good Evening";
+
+    const text = `${greeting} David, I came across your portfolio and I am interested in discussing a potential project with you. Could we schedule a call to talk further?`;
+
     return (
         <>
             <nav>
@@ -26,7 +39,7 @@ export default function Navbar() {
                     <Eye />
                     {/* <h1>Okoye David</h1> */}
                 </div>
-                <Button className="primary" onClick={openMenu}>Menu</Button>
+                <Button className="primary" onClick={openMenu}>{open ? "Close" : "Menu"}</Button>
 
             </nav>
 
@@ -41,9 +54,44 @@ export default function Navbar() {
                     </div>
 
                     <div className="navbox">
-                        <h3>dev.hype7@gmail.com</h3>
+                        <a href={`mailto:dev.hype7@gmail.com?subject=${encodeURIComponent("Project Inquiry From <Company Name>")}&body=${encodeURIComponent(text)}`}>
+
+                            <h3>dev.hype7@gmail.com</h3>
+                        </a>
+                        <div className="socials">
+                            <div className="socialWrap">
+                                <a href="https://www.instagram.com/dehypechief/">
+                                    <div className="socialBox">
+                                        <div className="socialIcon">
+                                            <Icon icon="basil:instagram-solid" width="24" height="24" />
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="https://www.linkedin.com/in/david-chinedu-okoye/">
+                                    <div className="socialBox">
+                                        <div className="socialIcon">
+                                            <Icon icon="ri:linkedin-fill" width="24" height="24" />                            </div>
+                                    </div>
+                                </a>
+                                <a href="https://x.com/deHypeChief/">
+                                    <div className="socialBox">
+                                        <div className="socialIcon">
+                                            <Icon icon="prime:twitter" width="18" height="18" />
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="http://github.com/deHypeChief">
+                                    <div className="socialBox">
+                                        <div className="socialIcon">
+                                            <Icon icon="mdi:github" width="24" height="24" />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </div >
             }
         </>
     )
