8471bcd6b95fae85ca64acd7576ba571b1f9c2fc
deHypeChief <dev.hype7@gmail.com>
2025-04-06 00:22:22 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -69,7 +69,7 @@ export default function Footer() {
                 <div className="socials">
                     <div className="mailAddress">
                         <a href={`mailto:davidchinedu.okoye@gmail.com?subject=${encodeURIComponent("Project Inquiry From <Company Name>")}&body=${encodeURIComponent(text)}`}>
-                            <h3>david...okoye@gmail.com</h3>
+                            <h3>david...@gmail.com</h3>
                         </a>
                         <div className="socialLine">
                             {/* <div className="innerLine"></div> */}
